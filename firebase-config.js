9a13a06cba184e7972475f0f07a8b55111e7dc14
Michele Marchetti <77664286+mikmark95@users.noreply.github.com>
2026-06-17 17:41:26 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,14 @@
 // 3) Copia qui la configurazione che Firebase ti mostra.
 // La configurazione Firebase non è una password: la sicurezza vera sta nelle regole Firestore.
 
-export const firebaseConfig = {
-  apiKey: "INSERISCI_API_KEY",
-  authDomain: "INSERISCI_PROJECT_ID.firebaseapp.com",
-  projectId: "INSERISCI_PROJECT_ID",
-  storageBucket: "INSERISCI_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "INSERISCI_SENDER_ID",
-  appId: "INSERISCI_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyDxJ33z3PungAedogFX1mk5-hxQk3fjADw",
+  authDomain: "lista-nascita-theo.firebaseapp.com",
+  projectId: "lista-nascita-theo",
+  storageBucket: "lista-nascita-theo.firebasestorage.app",
+  messagingSenderId: "972224539509",
+  appId: "1:972224539509:web:53036b24545cdab4b0413c",
+  measurementId: "G-6B81NZMRTH"
 };
 
 export const siteSettings = {
